bf3ee0c2c7bf3bf02d895c92d9525c7e16415d51
ModogTheDev <65304020+Modog500000@users.noreply.github.com>
2025-04-05 14:35:22 -0500

    🎉 INITAL COMMIT

---
+++ src/main/java/xyz/volcanobay/somber/Config.java
@@ -0,0 +1,36 @@
+package xyz.volcanobay.somber;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.config.ModConfigEvent;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
+// Demonstrates how to use Neo's config APIs
+@EventBusSubscriber(modid = Somber.MODID, bus = EventBusSubscriber.Bus.MOD)
+public class Config {
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+    private static final ModConfigSpec.IntValue MAGIC_NUMBER = BUILDER.comment("A magic number").defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
+
+    static final ModConfigSpec SPEC = BUILDER.build();
+
+    public static int magicNumber;
+
+
+    private static boolean validateItemName(final Object obj) {
+        return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(itemName));
+    }
+
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event) {
+        magicNumber = MAGIC_NUMBER.get();
+    }
+}
